200224c89649c55bda41fccdf7475a5cf3b6928f
Thomas E. Dickey <dickey@invisible-island.net>
2005-10-06 00:00:00 +0000

    Import xterm_204.orig.tar.gz

---
+++ version.h
@@ -0,0 +1,14 @@
+/* $XTermId: version.h,v 1.248 2005/08/05 01:25:40 tom Exp $ */
+/* $XFree86: xc/programs/xterm/version.h,v 3.115 2005/08/05 01:25:40 dickey Exp $ */
+
+/*
+ * These definitions are used to build the string that's printed in response to
+ * "xterm -version", or embedded in "xterm -help".  It usually indicates the
+ * version of X to which this version of xterm has been built.  The number in
+ * parentheses is my patch number (T.Dickey).
+ */
+#define XTERM_PATCH   204
+
+#ifndef __vendorversion__
+#define __vendorversion__ "XTerm"
+#endif
